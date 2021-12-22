682fe22fd88c4a5576d437aa36bc0056ddd74e1b
galinagumenyuk <gymenyuk.galina@gmail.com>
2021-12-22 18:15:55 +0200

    add Fetch and button LoadMore

--- src/App.js
+++ src/App.js
@@ -2,19 +2,40 @@ import React from "react";
 import "./App.css";
 import SearchBar from "./components/Searchbar";
 import ImageGallery from "./components/ImageGallery";
+import ButtonLoadMore from "./components/Button";
 
 class App extends React.Component {
-  state = { name: "" };
+  state = {
+    query: "",
+    results: [],
+    loadMore: false,
+  };
 
   handleFormSubmit = ({ name }) => {
-    this.setState({ name });
+    this.setState({ query: name });
+  };
+
+  handleLoadingUpd = (results) => {
+    this.setState({ results });
+    this.setState({ loadMore: false });
+  };
+
+  handleOnClick = (loadMore) => {
+    this.setState({ loadMore });
   };
 
   render() {
     return (
       <div>
         <SearchBar onSubmit={this.handleFormSubmit} />
-        <ImageGallery name={this.state.name} />
+        <ImageGallery
+          query={this.state.query}
+          onUpd={this.handleLoadingUpd}
+          loadMore={this.state.loadMore}
+        />
+        {this.state.results.length > 0 && (
+          <ButtonLoadMore onClick={this.handleOnClick} />
+        )}
       </div>
     );
   }
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -7,25 +7,42 @@ const KEY = '23837167-bf1b53cb947cc958b90463dad';
 
 class ImageGallery extends React.Component {
     state = {
-        image: null
+        results: [],
+        loading: false
     }
-    
+     
+    page = 1;
 
     componentDidUpdate(prevProps, prevState) {
-        if (prevProps.name !== this.props.name) {
-            fetch(`https://pixabay.com/api/?q=${this.props.name}&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`)
+        debugger;
+        if (!this.state.loading && (this.props.loadMore || (prevProps.query !== this.props.query))) {
+            this.setState({loading: true});
+            fetch(`https://pixabay.com/api/?q=${this.props.query}&page=${this.page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`)
                 .then(res => res.json())
-                .then (data => data.hits)
-                .then(image => this.setState({image} ));
+                .then(data => data.hits)
+                .then(results => {
+                    this.setState(prevState => ({
+                        results: prevState.results.concat(results)
+                    }));
+                })
+                .finally(() => {
+                    this.handleResults();
+                    this.setState({ loading: false });
+                    this.page += 1;
+                    console.log(this.page);
+                });
          }
-     }
+    }
     
+    handleResults = () => {
+         this.props.onUpd(this.state.results);
+    };
 
     render() {
         return (
             <Gallery>
-                {this.state.image &&
-                    this.state.image.map((item) =>  <Item image={item} />)
+                {this.state.results &&
+                    this.state.results.map((item) => <Item key={item.id} image={item} />)
                    }
             </Gallery>
         )
